7f605134ca0aafe35d303ee8009fe530f258c368
thjco <github@colla.de>
2022-10-16 19:39:12 +0200

    Provide example data.

--- meterreading.py
+++ meterreading.py
@@ -1,3 +1,36 @@
 import streamlit as st
+from datetime import datetime
+from meterreading_tools import *
 
-st.write("Meter Reading")
+
+st.set_page_config(page_title="Meter Reading", initial_sidebar_state="collapsed")
+st.title("Zählerstände")
+
+with st.sidebar:
+    if st.button("Daten löschen"):
+        drop_tables()
+
+    if st.button("Beispieldaten"):
+        set_example_data()
+
+conn = create_connection(DB_FILE)
+ensure_tables(conn)
+
+entries = select_all_entries(conn)
+
+tab_entry, tab_analysis, tab_data = st.tabs(["Eingabe", "Auswertung", "Daten"])
+
+with tab_entry:
+    with st.form("input"):
+        now = datetime.today()
+
+        r_date = st.date_input("Datum", value=now.date()) # value="2022/10/02") #
+        r_time = st.time_input("Uhrzeit", value=now.time())
+
+        gas = st.number_input("Gas", min_value=0., step=0.001, value=0.0, format="%.3f")
+
+        submitted = st.form_submit_button("Speichern")
+
+
+with tab_data:
+    st.dataframe(entries)
---
+++ meterreading_tools.py
@@ -0,0 +1,99 @@
+import sqlite3
+import json
+import pandas as pd
+
+
+DB_FILE = "meterreading.db"
+EXAMPLE_FILE = "mr-examples.json"
+
+ENSURE_MR_TABLE_SQL = """
+    CREATE TABLE IF NOT EXISTS mr (
+        id integer PRIMARY KEY,
+        rdate datetime NOT NULL,
+        gas float NOT NULL
+    );
+"""
+
+DROP_MR_TABLE = """
+    DROP TABLE IF EXISTS mr;
+"""
+
+MR_INSERT_SQL = """
+    INSERT INTO mr (rdate, gas)
+        VALUES(?, ?);
+"""
+
+MR_SELECT_SQL = """
+    SELECT id, rdate, gas FROM mr ORDER BY rdate ASC;
+"""
+
+
+def create_connection(db_file):
+    """ create a database connection to the SQLite database
+        specified by db_file
+    :param db_file: database file
+    :return: connection object or None
+    """
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+        return conn
+    except Error as e:
+        print(e)
+
+    return conn
+
+
+def ensure_tables(conn):
+    cur = conn.cursor()
+    cur.execute(ENSURE_MR_TABLE_SQL)
+    conn.commit()
+
+
+def drop_tables():
+    conn = create_connection(DB_FILE)
+    cur = conn.cursor()
+    cur.execute(DROP_MR_TABLE)
+    conn.commit()
+    conn.close()
+
+
+def create_entry(conn, entry):
+    """Put a new entry into the database
+    :param conn: connection object
+    :param entry: entry to be stored
+    """
+    values = (entry["rdate"], entry["gas"])
+    cur = conn.cursor()
+    cur.execute(MR_INSERT_SQL, values)
+    conn.commit()
+
+
+def select_all_entries(conn):
+    """Read all entries from the database table
+    :param: connection object
+    :return: pd.DataFrame
+    """
+    cur = conn.cursor()
+    cur.execute(MR_SELECT_SQL)
+    rows = cur.fetchall()
+
+    columns = "id rdate gas".split()
+    df = pd.DataFrame(rows, columns=columns).set_index("id")
+    return df
+
+
+def set_example_data():
+    with open(EXAMPLE_FILE) as f:
+        entries = json.load(f)
+    entries = pd.DataFrame(entries).sort_values("rdate")
+
+    drop_tables()
+
+    conn = create_connection(DB_FILE)
+    ensure_tables(conn)
+
+    for _, entry in entries.iterrows():
+        create_entry(conn, entry)
+
+    conn.close()
